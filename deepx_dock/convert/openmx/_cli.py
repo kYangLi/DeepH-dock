4e207990c7cbfe607c56e1614292331373aef069
zhaobh01 <zbh23@mails.tsinghua.edu.cn>
2026-03-23 20:48:46 +0800

    add: inject deeph hamiltonian to openmx scfout file

--- deepx_dock/convert/openmx/_cli.py
+++ deepx_dock/convert/openmx/_cli.py
@@ -73,3 +73,60 @@ def translate_openmx_to_deeph(
     translator.transfer_all_openmx_to_deeph()
     click.echo("[done] Translation completed successfully!")
 
+
+@register(
+    cli_name="to-openmx",
+    cli_help="Inject the DeepH predicted Hamiltonian into OpenMX scfout file.",
+    cli_args=[
+        click.argument(
+            'openmx_dir', type=click.Path(exists=True, file_okay=False),
+        ),
+        click.argument(
+            'deeph_dir', type=click.Path(exists=True, file_okay=False),
+        ),
+        click.argument(
+            'output_dir', type=click.Path(file_okay=False),
+        ),
+        click.option(
+            "--jobs-num", "-j", type=int, default=-1,
+            help="The parallel processing number, -1 for using all of the cores."
+        ),
+        click.option(
+            '--tier-num', '-t', type=int, default=0,
+            help="The tier number of the dataset, -1 for [deeph_dir], 0 for <deeph_dir>/<data_dirs>, 1 for <deeph_dir>/<tier1>/<data_dirs>, etc."
+        ),
+        click.option(
+            '--force', is_flag=True, help="Force to overwrite the existing files."
+        ),
+    ],
+)
+def translate_deeph_to_openmx(
+    openmx_dir: Path, deeph_dir: Path, output_dir: Path,
+    jobs_num: int, tier_num: int, force: bool,
+):
+    openmx_dir = Path(openmx_dir)
+    deeph_dir = Path(deeph_dir)
+    output_dir = Path(output_dir)
+    if not openmx_dir.is_dir():
+        raise click.ClickException(f"OpenMX data path '{openmx_dir}' is not a directory!")
+    if not deeph_dir.is_dir():
+        raise click.ClickException(f"DeepH data path '{deeph_dir}' is not a directory!")
+    if (not force) and output_dir.is_dir():
+        click.confirm(
+            f"The output data path '{output_dir}' already exists. Continue?",
+            abort=True
+        )
+    else:
+        output_dir.mkdir(parents=True, exist_ok=True)
+    #
+    from deepx_dock.convert.openmx.translate_deeph_to_openmx import DeepHToOpenMXTranslator
+    translator = DeepHToOpenMXTranslator(
+        openmx_data_dir=openmx_dir,
+        deeph_data_dir=deeph_dir,
+        output_dir=output_dir,
+        n_jobs=jobs_num,
+        n_tier=tier_num,
+    )
+    translator.transfer_all_deeph_to_openmx()
+    click.echo("[done] Translation completed successfully!")
+
